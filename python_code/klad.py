92ea77dcc8da8c6f8eaedf3e4aa956257f3da666
Jelle den Haan <jelle_den_haan@hotmail.nl>
2018-02-01 16:03:11 +0100

    updated files

--- python_code/klad.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-from datetime import datetime
-date_object = datetime.strptime('Jul 1 2014', '%b %d %Y')
-print(date_object)
--- python_code/klad2.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-
-d = dt.datetime.strptime("25-01-1973", "%d-%m-%Y")
-
-# Convert datetime object to date object.
-d = d.date()
-
-print(d.isoformat())
-# 1973-01-25
